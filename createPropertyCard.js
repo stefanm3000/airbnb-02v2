99161e46d8276a37355d61e6baa3025be2cb5bcf
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-10-28 16:07:57 +0100

    :construction:

--- createPropertyCard.js
+++ createPropertyCard.js
@@ -5,15 +5,15 @@ import {ratingAndIcon} from './ratingAndIcon.js';
 import {descriptionCard} from './descriptionCard.js';
 import {ppNightAndTotal} from './ppNightAndTotal.js';
 
-export function createPropertyCard(properties){
+export function createPropertyCard(property){
     
         let divGridAreas = document.createElement("div");
         divGridAreas.setAttribute('class', 'div-grid-areas');
 
-        divGridAreas.appendChild(imagesLinksButtons(properties.link, properties.imageUrl));
-        divGridAreas.appendChild(ratingAndIcon(properties.rating, properties.numOfReviews));
-        divGridAreas.appendChild(descriptionCard(properties.propSize, properties.location, properties.description));
-        divGridAreas.appendChild(ppNightAndTotal(properties.pricePerNight, properties.priceTotal));
+        divGridAreas.appendChild(imagesLinksButtons(property.link, property.imageUrl));
+        divGridAreas.appendChild(ratingAndIcon(property.rating, property.numOfReviews));
+        divGridAreas.appendChild(descriptionCard(property.propSize, property.location, property.description));
+        divGridAreas.appendChild(ppNightAndTotal(property.pricePerNight, property.priceTotal));
 
         return divGridAreas;
 }
